c07e623f5c31f844b92ba873e8e0861234811b32
Steve Rehm <steverehm@outlook.com>
2024-06-12 12:31:46 +0200

    clean main

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # react-challenges
 
-## Description :
-
-Implement a Stopwatch/timmer as shown below with start ,stop and reset button.
+1. timer
--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,9 @@
-import * as React from "react";
 import "./App.css";
 
 function App() {
-  const [isRunning, setIsRunning] = React.useState(false);
-  const [timer, setTimer] = React.useState({
-    minutes: 0,
-    seconds: 0,
-  });
-
-  React.useEffect(() => {
-    const timerFunction = setTimeout(function () {
-      if (isRunning) {
-        if (timer.seconds < 59) {
-          setTimer({
-            minutes: timer.minutes,
-            seconds: timer.seconds + 1,
-          });
-        } else {
-          setTimer({
-            minutes: timer.minutes + 1,
-            seconds: 0,
-          });
-        }
-      }
-    }, 1000);
-
-    return () => {
-      clearTimeout(timerFunction);
-    };
-  }, [timer, isRunning]);
-
-  const startTimer = () => {
-    setIsRunning(true);
-  };
-  const stopTimer = () => {
-    setIsRunning(false);
-  };
-  const resetTimer = () => {
-    setTimer({
-      minutes: 0,
-      seconds: 0,
-    });
-  };
   return (
     <div className="container">
-      <h1>Timer</h1>
-      <span> {timer.minutes} mins </span>
-      <span> {timer.seconds} secs</span>
-      <div>
-        <button onClick={startTimer}>Start</button>
-        <button onClick={stopTimer}>Stop</button>
-        <button onClick={resetTimer}>Reset</button>
-      </div>
+      <h1>React Challenges</h1>
     </div>
   );
 }
